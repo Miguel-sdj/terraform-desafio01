3ee753b440dae750b048a38a2a5c87a260d56c15
Miguel_sdj <miguelccarvalho1@gmail.com>
2024-09-11 16:44:27 -0300

    Adicionando parte 2 + pouco da parte 3

---
+++ sg/main.tf
@@ -0,0 +1,47 @@
+module "vpc" {
+  source = "../vpc"
+}
+
+resource "aws_security_group" "sg_public_ec2" {
+  vpc_id = aws_vpc.module.vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_private_ec2" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_public_ec2.id]
+  }
+}
+
+resource "aws_security_group" "sg_rds" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_private_ec2.id]
+  }
+}
+
+resource "aws_security_group" "sg_alb" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ vpc/main.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "172.31.0.0/16"
+}
+
+# Criação da Subnet Pública
+resource "aws_subnet" "subnet_public" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "172.31.1.0/24"
+ # sg
+  map_public_ip_on_launch = true
+}
+
+# Criação da Subnet private 
+resource "aws_subnet" "subnet_private" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "172.31.2.0/24"
+}
+
+# Criação da Subnet private 2 
+resource "aws_subnet" "subnet_private2" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "172.31.3.0/24"
+}
+
+# criando o internet gateway
+resource "aws_internet_gateway" "main_gw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# criando a rota para internet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_gw.id
+  }
+}
+
+# associando a subnet publica com a rota publica
+resource "aws_route_table_association" "public_subnet_assoc" {
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.public_rt.id
+}
